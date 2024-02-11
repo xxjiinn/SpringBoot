24bf868e922ea9eb18d66525c8a18a6c22922d2a
sungjin555555 <103707815+sungjin555555@users.noreply.github.com>
2024-02-12 00:53:14 +0900

    hi -> hii

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hii")
+    public String nceToMeetYou(Model model) {
+        model.addAttribute("username", "성진");
+        return "greeting"; // templates/greeting.mustache 파일을 찾아서 브라우저로 전송 !!
+    }
+
+}
