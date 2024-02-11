51a79e52d9afc8083bbe4f1135705081c0503120
sungjin555555 <103707815+sungjin555555@users.noreply.github.com>
2024-02-12 00:59:45 +0900

    hii -> hi

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
 
-    @GetMapping("/hii")
+    @GetMapping("/hi")
     public String nceToMeetYou(Model model) {
         model.addAttribute("username", "성진");
         return "greeting"; // templates/greeting.mustache 파일을 찾아서 브라우저로 전송 !!
