d8d9d319c0cb3a9ec822b5240a23185bd891031d
sungjin555555 <103707815+sungjin555555@users.noreply.github.com>
2024-02-14 20:11:09 +0900

    create object in entity class

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity // DB가 해당 객체를 인식 가능!
+public class Article {
+
+    // DB가 알 수 있는 규격화된 객체 생성
+    @Id // 대푯값을 지정! like a 주민등록번호
+    @GeneratedValue // 1, 2, 3, ... 자동 생성 어노테이션!
+    private Long id;
+    @Column // DB의 table 과 연결되도록
+    public String title;
+    @Column
+    public String content;
+}
