cd942c4f75b469f34fbe24cc841eda481a5dbfc7
sungjin555555 <103707815+sungjin555555@users.noreply.github.com>
2024-02-15 23:52:39 +0900

    model of username

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -9,14 +9,14 @@ public class FirstController {
 
     @GetMapping("/hi") // 주소창에 "/hi" 라는 주소를 입력하면 @GetMapping를 통해 여기로 찾아와서 해당 메서드 (niceToMeetYou())를 호출한다.
     public String niceToMeetYou(Model model) {
-        model.addAttribute("username", "주인"); // username이라는 변수의 값은 "주인장"
+        model.addAttribute("username", "주인장"); // username이라는 변수의 값은 "주인장"
         return "greeting"; // templates/greeting.mustache 파일을 찾아서 브라우저로 전송 !!
     }
 
     @GetMapping("/bye") // 주소창에 "/bye" 라는 주소를 입력하면 @GetMapping를 통해 여기로 찾아와서 해당 메서드 (seeyouNext())를 호출한다.
 
     public String seeyouNext(Model model) {
-        model.addAttribute("nickname", "개발자?"); // nickname 변수의 값은 "개발자"
+        model.addAttribute("nickname", "개발자"); // nickname 변수의 값은 "개발자"
         return "goodbye"; // templates/goodbye.mustache 파일을 찾아서 브라우저로 전송 !!
     }
 
