32dc9cb55ec4ada096a6050207c9ae0e97b8f182
sungjin555555 <103707815+sungjin555555@users.noreply.github.com>
2024-03-03 20:45:39 +0900

    edit FirstController.java

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -13,7 +13,7 @@ public class FirstController {
         return "greeting"; // templates/greeting.mustache 파일을 찾아서 브라우저로 전송 !!
     }
 
-    @GetMapping("/bye!") // 주소창에 "/bye" 라는 주소를 입력하면 @GetMapping를 통해 여기로 찾아와서 해당 메서드 (seeyouNext())를 호출한다.
+    @GetMapping("/bye") // 주소창에 "/bye" 라는 주소를 입력하면 @GetMapping를 통해 여기로 찾아와서 해당 메서드 (seeyouNext())를 호출한다.
 
     public String seeyouNext(Model model) {
         model.addAttribute("nickname", "개발자!"); // nickname 변수의 값은 "개발자"
